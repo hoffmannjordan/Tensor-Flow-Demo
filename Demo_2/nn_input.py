df6b5faff1cdd05ab767b0b6147d13b8f5875c94
Jordan Hoffmann <jhoffmann@g.harvard.edu>
2018-11-30 16:49:34 -0500

    continued working

--- Demo_2/nn_input.py
+++ Demo_2/nn_input.py
@@ -1,17 +1,30 @@
 import numpy as np
+from scipy.io import loadmat
+from __init__ import *
 
-def nn_input(key):
-    solution_data = training_data(key)
+def nn_input(id):
+	data = loadmat('Sol_'+str(id)+'.mat')
+	Mat_1 = np.array(data['Expression1'])
+	Mat_2 = np.array(data['Expression2'])
+	Mat_3 = np.array(data['Expression3'])
+	mats = [Mat_1 , Mat_2, Mat_3]
+	side = np.genfromtxt('Side_'+str(id)+'.csv',delimiter=',')
+	
+	window_size = Mat_1.shape
 
-    window_size = solution_data.shape
-
-    X_main = np.zeros((window_size[0], window_size[1], FEATURES))
+	X_main = np.zeros((window_size[0], window_size[1], FEATURES))
 
 
-    X_side = np.zeros(SIDE_FEATURES)
-    X_side[0] = 
-
-    Y = 
-
+	X_side = np.zeros(SIDE_FEATURES)
+	for i in range(SIDE_FEATURES):
+		X_side[i] = side[i]
+	#print np.shape(X_main)
+	for i in range(FEATURES):
+		X_main[:,:,i] += mats[i]
+	Y  = np.genfromtxt('Sol_'+str(id)+'.csv',delimiter=',') 
+	#print Y
     return [X_main, X_side], Y
 
+
+# if __name__ == '__main__':
+# 	nn_input(1)
